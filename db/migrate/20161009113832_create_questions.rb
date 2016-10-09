8a3a15d5f05a06611e30cb56979a460ed27ba760
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-10-09 21:20:04 +0900

    create questions_table

--- db/migrate/20161009113832_create_questions.rb
+++ db/migrate/20161009113832_create_questions.rb
@@ -1,7 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :content
+      t.references :user
+      t.string :image
+      t.integer :contribution
       t.timestamps null: false
     end
   end
