9c7d15a33f67f1777841fdf8adec97ca01e66713
ikeda <ikebright@gmail.com>
2016-10-15 18:59:57 +0900

    add index for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
 #トップ画面
   root 'top#index'
 
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-
+  resources :users, only: [:index]
 #質問機能
   resources :questions
 
