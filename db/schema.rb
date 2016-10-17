32fe8e2ab82160b8f22b037438bdacac3f7ff26c
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2016-10-18 00:54:20 +0900

    【回答機能】モデルを作成する

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017153931) do
+ActiveRecord::Schema.define(version: 20161009125542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "answers", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "contribution"
-    t.string   "image"
-    t.integer  "user_id_id"
-    t.integer  "question_id_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
