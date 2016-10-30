a7d812000befb23a4a95b646f91340cb514c2427
ikeda <ikebright@gmail.com>
2016-10-30 13:40:51 +0900

    Revert "ユーザ詳細"
    This reverts commit 0ff037cfaa8bcec3fe0efffcad949233f514bcfa.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index]
 #質問機能
   resources :questions do
     resources :answers
