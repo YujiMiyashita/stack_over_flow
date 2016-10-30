30d2b7784440c064c639cbe7bb91c134636da82d
ikeda <ikebright@gmail.com>
2016-10-30 13:45:57 +0900

    ユーザ詳細追加及びuploadersをgitignoreに追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 #質問機能
   resources :questions do
     resources :answers
