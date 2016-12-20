5d119e409a7c891a8cf5b52010b90c9a0ebf05fd
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2016-12-20 12:01:20 +0900

    【投票機能】回答に対しても投票できるようにする

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
-  before_action :set_vote
+  before_action :set_question_vote, only: [:good, :bad]
+  before_action :set_answer_vote, only: [:answer_good, :answer_bad]
 
   def good
     good = current_user.votes.build(question_id: @question.id)
@@ -15,8 +16,26 @@ class VotesController < ApplicationController
     redirect_to question_path(@question.id)
   end
 
+  def answer_good
+    good = current_user.votes.build(answer_id: @answer.id)
+    good.contribution = 1
+    good.save
+    redirect_to question_path(@answer.question.id)
+  end
+
+  def answer_bad
+    bad = current_user.votes.build(answer_id: @answer.id)
+    bad.contribution = 2
+    bad.save
+    redirect_to question_path(@answer.question.id)
+  end
+
   private
-    def set_vote
+    def set_question_vote
       @question = Question.find(params[:question_id])
+    end
+
+    def set_answer_vote
+      @answer = Answer.find(params[:answer_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 #投票機能
 post 'good/:question_id' => 'votes#good', as:'good'
 post 'bad/:question_id' => 'votes#bad', as:'bad'
+post 'good_answer/:answer_id' => 'votes#answer_good', as:'answergood'
+post 'bad_answer/:answer_id' => 'votes#answer_bad', as:'answerbad'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
