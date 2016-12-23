1936513291f2c8ec2268e11ac9d9500e10eed30f
ikeda <ikebright@gmail.com>
2016-12-24 00:53:20 +0900

    タグ機能追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,5 @@ class Question < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :question_tags
   has_many :tags, through: :question_tags
+  acts_as_ordered_taggable_on :tags
 end
--- app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ActiveRecord::Base
-  has_many :tag_questions
-  has_many :questions, through: :tag_questions
-end
