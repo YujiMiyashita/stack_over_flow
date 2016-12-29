b849c02741449a4645728f228e6cfab907202fe8
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2016-12-30 02:09:51 +0900

    【お気に入り機能】Ajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
 
   def create
+    @question = Question.find(params[:favorite][:question_id])
     current_user.favorites.create(question_id: params[:favorite][:question_id])
-    redirect_to questions_path
   end
 
   def destroy
+    @question = Question.find(params[:favorite][:question_id])
     favorite = current_user.favorites.find_by(question_id: params[:favorite][:question_id])
     favorite.destroy
-    redirect_to questions_path
   end
 end
