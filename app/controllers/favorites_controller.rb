72950d88d89395e94bd4ee41d51c09e7ec1db9ae
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2016-12-31 17:39:38 +0900

    【お気に入り機能】Ajax化-修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_question
 
   def create
     @question = Question.find(params[:favorite][:question_id])
-    current_user.favorites.create(question_id: params[:favorite][:question_id])
+    current_user.favorites.create(question_id: @question.id)
   end
 
   def destroy
     @question = Question.find(params[:favorite][:question_id])
-    favorite = current_user.favorites.find_by(question_id: params[:favorite][:question_id])
+    favorite = current_user.favorites.find_by(question_id: @question.id)
     favorite.destroy
   end
+
+  private
+    def set_question
+      @question = Question.find(params[:favorite][:question_id])
+    end
 end
