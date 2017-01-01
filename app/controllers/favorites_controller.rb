96268aa29b164f3b3831b1e02ae99bbd070c2667
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2017-01-01 16:37:40 +0900

    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,10 @@ class FavoritesController < ApplicationController
   before_action :set_question
 
   def create
-    @question = Question.find(params[:favorite][:question_id])
     current_user.favorites.create(question_id: @question.id)
   end
 
   def destroy
-    @question = Question.find(params[:favorite][:question_id])
     favorite = current_user.favorites.find_by(question_id: @question.id)
     favorite.destroy
   end
