9adbb34df28f08909bdf35d6404bd3330398f2fa
CINO SAKAIDA <cino@CINO-no-MacBook-Air.local>
2017-01-12 03:45:25 +0900

    マイページからお気に入りした質問の一覧を見られるようにする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,5 @@ class TopController < ApplicationController
   def index
     @questions = Question.all
     @tags = Tag.order("taggings_count DESC").limit(10)
-    @title = "上位の質問"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @title = "お気に入りの質問"
   end
 end
